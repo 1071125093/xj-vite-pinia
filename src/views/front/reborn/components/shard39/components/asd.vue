03b218e9b109f45aa879966d064940011c0e868c
潇军 <http://git.aihuoshi.net/huangxj>
2023-04-12 17:41:30 +0800

    123

--- src/views/front/reborn/components/shard39/components/asd.vue
+++ src/views/front/reborn/components/shard39/components/asd.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: XiaoJun
+ * @Date: 2023-04-06 12:54:00
+ * @LastEditors: XiaoJun
+ * @LastEditTime: 2023-04-12 17:10:17
+ * @Description: 组件功能
+ * @FilePath: /xj-vite-pinia/src/views/front/reborn/components/shard39/components/asd.vue
+-->
 <script lang="ts" setup>
 // #region ********** 库&组件等引入 start **************/
 // #endregion ******* 库&组件等引入 ~end~ **************/
@@ -9,7 +17,7 @@
 // #endregion ******* 测试区域 ~end~ **************/
 </script>
 <template>
-  <div class="default_class">填充内容</div>
+  <div class="default_class">填充内容123</div>
 </template>
 <style lang="less" scoped>
 .default_class {
